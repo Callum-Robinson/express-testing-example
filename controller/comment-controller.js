08ea5553c8a6d8783b22eb428c2f181419cec61e
MrWalshyType2 <mwalsh@qa.com>
2022-04-08 22:08:35 +0100

    refactored comment controller

--- controller/comment-controller.js
+++ controller/comment-controller.js
@@ -1,63 +1,44 @@
+const HttpError = require('../errors/http-error');
 const commentService = require('../services/comment-service');
 const Comment = require('../model/comment');
+const PostNotFoundError = require('../errors/post-not-found');
+const CommentNotFoundError = require('../errors/comment-not-found');
 
 module.exports = {
     read: async (req, res, next) => {
-        try {
-            const comments = await commentService.read();
-            res.status(200).json(comments);
-        } catch (err) {
-            next(err);
-        }
+        const comments = await commentService.read().catch(next);
+        res.status(200).json(comments);
     },
 
     readById: async(req, res, next) => {
-        try {
-            const comment = await commentService.readById(req.params.id);
+        const comment = await commentService.readById(req.params.id).catch(next);
 
-            if (!comment) {
-                const err = new Error(`Comment not found with id ${req.params.id}`);
-                err.statusCode = 404;
-                return next(err);
-            }
-
-            res.status(200).json(comment);
-        } catch (err) {
-            next(err);
+        if (!comment) {
+            const err = new HttpError(`Comment not found with id ${req.params.id}`, 404);
+            return next(err);
         }
+        res.status(200).json(comment);
     },
 
     create: async(req, res, next) => {
-        try {
-            const comment = await commentService.create(new Comment(req.body));
-            res.status(201).json(comment);
-        } catch (err) {
-            next(err);
-        }
+        const comment = await commentService.create(new Comment(req.body)).catch(err => {
+            if (typeof err === PostNotFoundError) return next(new HttpError(err, 404));
+            return next(err);
+        });
+        res.status(201).json(comment);
     },
 
     update: async(req, res, next) => {
-        try {
-            const comment = await commentService.update(new Comment(req.body));
-            res.status(200).json(comment);
-        } catch (err) {
-            next(err);
-        }
+        const comment = await commentService.update(new Comment(req.body)).catch(next);
+        res.status(200).json(comment);
     },
 
     deleteById: async(req, res, next) => {
-        try {            
-            const comment = await commentService.deleteById(req.params.id);
-
-            if (!comment) {
-                const err = new Error(`Comment not found with id ${req.params.id}`);
-                err.statusCode = 404;
-                return next(err);
-            }
-
-            res.status(200).json(comment);
-        } catch (err) {
-            next(err);
-        }
+        const comment = await commentService.deleteById(req.params.id).catch(err => {
+            if (typeof err === CommentNotFoundError) return next(new HttpError(err, 404));
+            else if (typeof err === PostNotFoundError) return next(new HttpError(err, 404));
+            else return next(err);
+        });
+        res.status(200).json(comment);
     }
 }
